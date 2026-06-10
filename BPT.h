257e27cfc6c2814080678870ea4e181202dd7185
ShieRoZ0415 <1417699502@qq.com>
2026-06-10 13:05:40 +0800

    perf: expand cache size

--- BPT.h
+++ BPT.h
@@ -35,7 +35,7 @@ private:
     Header h;
     Less less;
 
-    static const int CACHE_SIZE = 32;
+    static const int CACHE_SIZE = 64;
 
     Node cacheNode[CACHE_SIZE];
     int cachePos[CACHE_SIZE];
